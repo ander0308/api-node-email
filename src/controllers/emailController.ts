d3c433acc7bbe3c6a64e36811d495f7c06a7a414
Anderson Santana <ander0308@yahoo.com.br>
2023-02-07 10:47:41 -0300

    feat: config mail transport

--- src/controllers/emailController.ts
+++ src/controllers/emailController.ts
@@ -3,6 +3,14 @@ import nodemailer from 'nodemailer'
 
 export const contato = (req: Request, res: Response) => {
     // Passo 1: Configurar o transporter
+    let transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: "9e8b4ce341e223",
+                pass: "fb7e5c17305dcf"
+            }
+        });
     // Passo 2 : Configurar a mensagem
     // Passo 3: Enviar a mensagem
 
