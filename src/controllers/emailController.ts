e1a7357ba53f34e27aad9038bc844d6c43bef845
Anderson Santana <ander0308@yahoo.com.br>
2023-02-07 11:03:35 -0300

    feat: config message and send email

--- src/controllers/emailController.ts
+++ src/controllers/emailController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import nodemailer from 'nodemailer'
 
-export const contato = (req: Request, res: Response) => {
+export const contato = async (req: Request, res: Response) => {
     // Passo 1: Configurar o transporter
     let transport = nodemailer.createTransport({
             host: "sandbox.smtp.mailtrap.io",
@@ -12,7 +12,17 @@ export const contato = (req: Request, res: Response) => {
             }
         });
     // Passo 2 : Configurar a mensagem
+    let message = {
+        from: 'Anderson Santana <anderson@gmail.com>',
+        to: 'suporte@gmail.com',
+        // replyTo: '',
+        subject: 'Assunto do email',
+        html: 'Opa <strong>Anderson</strong> como está?',
+        text: 'Opa Anderson, como está'
+    }
     // Passo 3: Enviar a mensagem
+    let info = await transport.sendMail(message)
+    console.log("Info: ", info)
 
-    res.json({contato: true});
+    res.json({success: true});
 }
